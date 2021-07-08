ff980c932c026efde90ef01be36fec98f255ac43
Tinuade Adeleke <30666999+tinumide@users.noreply.github.com>
2021-07-08 23:26:52 +0100

    Time: 28 ms (80.99%), Space: 14.5 MB (11.04%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,10 +1,70 @@
 class Solution:
-    @lru_cache
+    
     def climbStairs(self, n: int) -> int:
+        
+        dp = [-1 for i in range(n+1)]
+        return self.helper(n, dp)
+        
+        
+    def helper(self, n, dp):
+        
         if n == 0:
             return 1
+        
         if n < 0:
             return 0
-        distinctWays = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return distinctWays
+        
+        if dp[n] != -1:
+            return dp[n]
+        
+        ways = self.helper(n-1, dp) + self.helper(n - 2, dp)  
+        
+        dp[n] = ways
+        
+        return dp[n]
+    
+        
+        
+        
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if n == 0:
+        #     return 1
+        # if n < 0:
+        #     return 0
+        # distinctWays = self.climbStairs(n-1) + self.climbStairs(n-2)
+        # return distinctWays
         
